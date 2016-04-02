d82889f05dec1d8b69da24e7c8807305e1b61d78
Adrian Deleon <amd87@students.uwf.edu>
2016-04-02 02:37:30 -0500

    Update Map and Tile, Add Nation.java

--- NationBuilderClient/src/Map/Tile/Tile.java
+++ NationBuilderClient/src/Map/Tile/Tile.java
@@ -1,10 +1,12 @@
 package Map.Tile;
+import Map.Map;
 
 public class Tile {
-    public Nation owner;
-    private boolean darkFlag;
-    private boolean defendFlag;
-    private Nation attacker;
+    private Map map;
+    public Nation owner; //nation which owns tile
+    private boolean darkFlag; //flag for dark zone tiles, which allow pvp
+    private boolean defendFlag; //flag for defending against attack
+    private Nation attacker; //nation attacking tile
 
     public Nation getOwner() {
         return owner;
@@ -38,14 +40,55 @@ public class Tile {
         this.attacker = attacker;
     }
 
-    public void declareBattle(Nation attacker, Tile defender){
-        defender.setAttacker(attacker);
-        server.pushQuestions();
+    public void declareBattle(Nation attacker, Tile defender, int cost){
+        if(!defender.isDarkFlag()){ //if tile isn't a dark zone tile
+            displayError("notDarkZoneTile"); //placeholder for actual error displaying
+            return;
+        }
+        if(defender.isDefendFlag()){ //if time is already in a battle
+            displayError("tileAlreadyInBattle");
+            return;
+        }
+        if(attacker.getResources() > cost){ //if nation can afford battle
+            attacker.setResources(attacker.getResources - cost); //remove resources for cost
+            defender.setAttacker(attacker);
+            defender.setDefendFlag(true);
+            server.pushNationQuestions(attacker, 1); //for each student in nation push 1 question
+            server.pushNationQuestions(defender.getOwner());
+        }
+        else{
+            displayError("notEnoughResources");
+            return;
+        }
     }
 
-    public void resolveBattle(Nation attacker, Tile)
+    public void resolveBattle(Tile defender, boolean defenderWin){
+        if(!defenderWin){
+            defender.setOwner(defender.getAttacker());
+        }
+        defender.setAttacker(null);
+        defender.setDefendFlag(false);
+    }
 
-    public Tile(Nation owner, boolean darkFlag, boolean defendFlag, Nation attacker){
+    public void buyTile(Nation buyer, Map map, int x, int y, int cost){
+        Tile product = map.getTime(x,y);
+        if(product.getOwner() != NULL){
+            displayError("tileAlreadyOwned");
+        }
+
+        if(buyer.getResources() > cost){ //if nation can afford tile
+            buyer.setResources(buyer.getResources() - cost); //remove resources for cost
+            Tile product = map.getTile(x,y); //retrieve desired tile
+            product.setOwner(buyer); //buyer now owns tile
+        }
+        else{
+            displayError("notEnoughResources");
+            return;
+        }
+    }
+
+    public Tile(Map map, Nation owner, boolean darkFlag, boolean defendFlag, Nation attacker){
+        this.map = map;
         this.owner = null;
         this.darkFlag = false;
         this.defendFlag = false;
