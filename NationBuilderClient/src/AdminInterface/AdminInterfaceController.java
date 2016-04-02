1a030d1ccd5cc6fdacba2a525ea771bceca6de08
James Davis <jedwarddavis902@gmail.com>
2016-04-02 10:47:33 -0500

    Added Admin Page fixed Logic Page

--- NationBuilderClient/src/AdminInterface/AdminInterfaceController.java
+++ NationBuilderClient/src/AdminInterface/AdminInterfaceController.java
@@ -1,11 +1,51 @@
 package AdminInterface;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by crims_000 on 4/1/2016.
  */
 public class AdminInterfaceController {
 
+    private class TableData{
+        public String team;
+        public int resourceAmount;
+        public int tileAmount;
 
+        public TableData(String team, int resourceAmount, int tileAmount){
+            this.team = team;
+            this.resourceAmount = resourceAmount;
+            this.tileAmount = tileAmount;
+        }
+    }
+    public TableView<TableData> dataTable;
+    public Button mapButton;
+
+    public void openMap(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MapRender/MapRender.fxml"));
+        AnchorPane root = new AnchorPane();
+        root.getChildren().clear();
+        root.getChildren().add(loader.load());
+
+        Stage stage = new Stage();
+        stage.setTitle("Map Viewer");
+        stage.setScene(new Scene(root,500,500));
+        stage.show();
+    }
+
+    public void loadInterface(){
+        //Temp code will get from data base later
+        ObservableList<TableData> data = FXCollections.observableArrayList(new TableData("Team A",0,0),new TableData("Team B",0,0),new TableData("Team C",0,0),new TableData("Team D",0,0));
+        dataTable.setItems(data);
+    }
 }
--- NationBuilderClient/src/Login/LoginController.java
+++ NationBuilderClient/src/Login/LoginController.java
@@ -1,5 +1,6 @@
 package Login;
 
+import AdminInterface.AdminInterfaceController;
 import StudentInterface.StudentInterfaceController;
 import WebUtilities.LoginReq;
 import WebUtilities.LoginRes;
@@ -42,7 +43,18 @@ public class LoginController {
             System.out.println(loginResponse.success);
             System.out.println(loginResponse.admin);
 
+            int sceneSizeX = 800;
+            int sceneSizeY = 800;
             if(loginResponse.success == true && loginResponse.admin == true){
+                Stage stage = (Stage) rootPane.getScene().getWindow();
+                stage.close();
+                FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("AdminInterface/AdminInterface.fxml"));
+                Parent root = loader.load();
+                stage.setTitle("[Enter Game Name Here] Admin Console");
+                stage.setScene(new Scene(root, sceneSizeX, sceneSizeY));
+                stage.show();
+                AdminInterfaceController controller = loader.getController();
+                controller.loadInterface();
 
             }else if(loginResponse.success == true && loginResponse.admin == false){
                 Stage stage = (Stage) rootPane.getScene().getWindow();
@@ -50,11 +62,10 @@ public class LoginController {
                 FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("StudentInterface/StudentInterface.fxml"));
                 Parent root = loader.load();
                 stage.setTitle("[Enter Game Name Here] Student Console");
-                stage.setScene(new Scene(root, 800, 800));
+                stage.setScene(new Scene(root, sceneSizeX, sceneSizeY));
                 stage.show();
                 StudentInterfaceController controller = loader.getController();
                 controller.loadInterface();
-                System.out.println("Here");
             }
             clientSocket.close();
         }catch(Exception e) {
