41f5c2bdd681d3bec1b7922e6a2c7d7b38c30a95
bmp18 <bmp18@students.uwf.edu>
2016-04-02 01:02:08 -0500

    Working on Login

--- NationBuilderClient/src/Login/LoginController.java
+++ NationBuilderClient/src/Login/LoginController.java
@@ -1,4 +1,36 @@
 package Login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class LoginController {
+
+    private static final InetAddress HOST = ;
+    private static final int PORT = 3000;
+    public Button loginButton;
+    public TextField userNameField;
+    public TextField passwordTextField;
+    public void loginCheck(ActionEvent actionEvent) {
+        LoginReq login = new LoginReq();
+
+        login.username = userNameField.getText();
+        login.password = passwordTextField.getText();
+
+        try{
+            Socket clientSocket = new Socket(HOST, PORT);
+            ObjectOutputStream outToServer = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
+
+            outToServer.writeObject(login);
+        }catch(Exception e) {
+
+        }
+    }
 }
---
+++ NationBuilderClient/src/Login/LoginReq.java
@@ -0,0 +1,9 @@
+package Login;
+
+/**
+ * Created by poebr_000 on 4/2/2016.
+ */
+public class LoginReq {
+    String username;
+    String password;
+}
