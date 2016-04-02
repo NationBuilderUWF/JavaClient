0a817c17e4abe786790076bb8ffb86235aa62c42
James Davis <jedwarddavis902@gmail.com>
2016-04-02 01:01:25 -0500

    Added files
    Added file paths and generated gui code for student interface

---
+++ NationBuilderClient/src/StudentInterface/StudentInterfaceController.java
@@ -0,0 +1,20 @@
+package StudentInterface;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by crims_000 on 4/1/2016.
+ */
+public class StudentInterfaceController {
+    public void questionAction(ActionEvent actionEvent) {
+    }
+
+    public void buyTileFunction(ActionEvent actionEvent) {
+    }
+
+    public void battleFunction(ActionEvent actionEvent) {
+    }
+
+    public void voidFunction(ActionEvent actionEvent) {
+    }
+}
